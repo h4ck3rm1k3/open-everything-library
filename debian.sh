a26df7495a38fb6b440e6d1a2559531e1bfc73bd
James Michael DuPont <jamesmikedupont@gmail.com>
2016-02-26 02:42:01 +0000

    debian

---
+++ debian.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-stdeb	python-all-dev devscripts python3-six python3-setuptools-git python3-setuptools python3-requests python3-rdflib python3-pymongo python3-simplejson python3-nltk python3-lxml python3-sparqlwrapper python3-urllib3  python3-sh
+sudo apt-get build-dep python-stdeb	python-all-dev devscripts python3-six python3-setuptools-git python3-setuptools python3-requests python3-rdflib python3-pymongo python3-simplejson python3-nltk python3-lxml python3-sparqlwrapper python3-urllib3  python3-sh
+#python-withsqlite #python-billiard
+#python3-deap
+#python3-logutils
+#python3-httplib2
+#python3-libarchive-c
+#python3-csvkit
